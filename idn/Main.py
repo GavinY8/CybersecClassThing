d5a563813d6694ffb9f314c6b2172476ef326287
GavinY8 <gaviny8@nycstudents.net>
2026-01-11 22:35:22 -0500

    qweqwe

---
+++ idn/Main.py
@@ -0,0 +1,8 @@
+character1 = 'a'
+unicode_value1 = ord(character1)
+print(f"The Unicode value of '{character1}' is {unicode_value1}")
+
+
+character2 = 'а'
+unicode_value2 = ord(character2)
+print(f"The Unicode value of '{character2}' is {unicode_value2}")
